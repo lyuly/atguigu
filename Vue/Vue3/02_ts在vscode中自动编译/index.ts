dbf7718452aaf41f6fb65e23386bbfeca7d32617
lyuly <liumike567@gmail.com>
2021-03-07 07:33:30 +0800

    update

---
+++ Vue/Vue3/02_ts在vscode中自动编译/index.ts
@@ -0,0 +1,7 @@
+(()=>{
+  function sayHi(str) {
+    return '啊，你好'+str
+  }
+  let text = '小甜甜'
+  console.log(sayHi(text));
+})()
---
+++ Vue/Vue3/02_ts在vscode中自动编译/js/index.js
@@ -0,0 +1,7 @@
+(function () {
+    function sayHi(str) {
+        return '啊，你好' + str;
+    }
+    var text = '小甜甜';
+    console.log(sayHi(text));
+})();
